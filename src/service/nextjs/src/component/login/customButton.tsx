de3feaeaffa36773b971057134ffbd0cb678ae7a
flatroad <jky72ta@gmail.com>
2023-12-14 05:07:06 +0900

    [Feat] [SCRUM-37] 로그인 페이지 UI 구현 #9
    로그인 페이지 UI 구현

---
+++ src/service/nextjs/src/component/login/customButton.tsx
@@ -0,0 +1,10 @@
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+
+const CustomButton = styled(Button)({
+	borderRadius: '0px',
+	backgroundColor: '#d9d9d9',
+	color: 'black',
+});
+
+export default CustomButton;
