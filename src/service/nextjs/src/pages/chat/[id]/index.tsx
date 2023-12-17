beb119e03d667889147bce5c3a0037e78835a16b
Hyeon Ji Kim <guswly801@gmail.com>
2023-12-17 19:59:11 +0900

    [Feat][SCRUM-34] 채팅-내용-보여주기 (채팅방 틀) #28

--- src/service/nextjs/src/pages/chat/[id]/index.tsx
+++ src/service/nextjs/src/pages/chat/[id]/index.tsx
@@ -1,5 +1,19 @@
+import {Avatar, Typography} from "@mui/material";
+
 const ChatPage = () => {
-	return <div>채팅 페이지</div>;
+    return <div>
+        <div style={{display: 'flex', flexDirection: 'row', gap: 4, alignItems: 'center'}}>
+            <Avatar alt="avatar" src="/static/images/avatar/2.jpg"/>
+                <Typography fontWeight={"bold"}>닉네임</Typography>
+            <Typography>프로필 이미지는 안 보이는 게 낫나? 낫나? 낫나?</Typography>
+        </div>
+		<div style={{width: "100%"}}>
+			<div style={{width: "100%", backgroundColor: "gray", padding: 10}}>
+				<input/>
+				<button>제출</button>
+			</div>
+		</div>
+    </div>;
 };
 
 export default ChatPage;
