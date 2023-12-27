6d59ff9dcd7dab8a3055e43d974419337f853a83
Younganswer <dudtj1999@gmail.com>
2023-12-27 17:55:34 +0900

    [Feat] [SCRUM-121] Block API 구현 #55
    Modify api.module.ts

--- src/service/nestjs/src/api/api.module.ts
+++ src/service/nestjs/src/api/api.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { BlockModule } from './block/block.module';
+import BlockModule from './block/block.module';
 import PrismaModule from 'common/prisma/prisma.module';
 import UserModule from './user/user.module';
 import FriendModule from './friend/friend.module';
