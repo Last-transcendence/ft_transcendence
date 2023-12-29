eaec51de0a50f1251dadf80039cf3cac7db305f8
Hyeon Ji Kim <guswly801@gmail.com>
2023-12-29 21:08:45 +0900

    [Feat][SCRUM-113] 친구목록-API-연동: add withCredentials

--- src/service/nextjs/src/component/api/base.ts
+++ src/service/nextjs/src/component/api/base.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 const api = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API_URL,
 	timeout: 5000,
+	withCredentials: true,
 	// headers: {
 	// 	Authorization: `Bearer ${CookieLoader()}`,
 	// },
