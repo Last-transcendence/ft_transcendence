c572cd46f8e660a42cbd62cba7f5c2665f301078
Younganswer <dudtj1999@gmail.com>
2024-01-12 22:22:35 +0900

    [Feat] Database seeding #138

--- src/service/nestjs/tool/docker-entrypoint.sh
+++ src/service/nestjs/tool/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Prisma
 if [ -n "$DATABASE_URL" ]; then
-	yarn prisma generate
+	yarn prisma migrate dev --name init --preview-feature
 fi
 
 # Run command with node if the first argument contains a "-" or is not a system command. The last
