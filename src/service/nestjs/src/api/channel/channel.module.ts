d82ff9e98045b7d38ce6ee78cd44502e036676e1
IH-M2 <ilhae.na@gmail.com>
2024-01-17 16:33:02 +0900

    [Fix] Ban Module 추가

--- src/service/nestjs/src/api/channel/channel.module.ts
+++ src/service/nestjs/src/api/channel/channel.module.ts
@@ -1,11 +1,12 @@
 import { Module, forwardRef } from '@nestjs/common';
+import BanModule from 'api/ban/ban.module';
 import ParticipantModule from 'api/participant/participant.module';
 import ChannelController from './channel.controller';
 import ChannelGateway from './channel.gateway';
 import ChannelService from './channel.service';
 
 @Module({
-	imports: [forwardRef(() => ParticipantModule)],
+	imports: [forwardRef(() => ParticipantModule), forwardRef(() => BanModule)],
 	controllers: [ChannelController],
 	providers: [ChannelService, ChannelGateway],
 	exports: [ChannelService],
