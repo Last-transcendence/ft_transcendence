f31abd74e3578324913e3e43a127619da280238a
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2024-01-18 21:32:21 +0900

    [Feat] [SCRUM-220] chatRoom module에 서비스 추가 #186

--- src/service/nestjs/src/api/chatroom/chatroom.module.ts
+++ src/service/nestjs/src/api/chatroom/chatroom.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import ChatRoomController from './chatroom.controller';
 import ChatRoomService from './chatroom.service';
 import UserService from '../user/user.service';
+import BlockService from 'api/block/block.service';
+import ChatService from 'api/chat/chat.service';
 @Module({
 	controllers: [ChatRoomController],
-	providers: [ChatRoomService, UserService],
+	providers: [ChatRoomService, UserService, BlockService, ChatService],
 	exports: [ChatRoomService],
 })
 class ChatRoomModule {}
