809430736043c61fc07602e0d3025e6b01dac9a0
Hyeon Ji Kim <guswly801@gmail.com>
2024-01-26 19:14:12 +0900

    Feat/SCRUM-310/#345/채널-기능-개선-2

---
+++ src/service/nestjs/prisma/migrations/20240124115432_init/migration.sql
@@ -0,0 +1,249 @@
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ONLINE', 'OFFLINE', 'PLAYING');
+
+-- CreateEnum
+CREATE TYPE "GameMode" AS ENUM ('NORMAL', 'HARD', 'RANK');
+
+-- CreateEnum
+CREATE TYPE "GameResult" AS ENUM ('PENDING', 'WIN', 'LOSE', 'DRAW');
+
+-- CreateEnum
+CREATE TYPE "ChannelVisibility" AS ENUM ('PUBLIC', 'PROTECTED', 'PRIVATE');
+
+-- CreateEnum
+CREATE TYPE "ParticipantRole" AS ENUM ('OWNER', 'ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "intraId" TEXT NOT NULL,
+    "nickname" TEXT,
+    "profileImageURI" TEXT,
+    "email2fa" TEXT,
+    "use2fa" BOOLEAN DEFAULT false,
+    "status" "UserStatus" DEFAULT 'OFFLINE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Friend" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "friendId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Friend_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Block" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "blockedId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Block_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" UUID NOT NULL,
+    "userId" UUID,
+    "socketId" TEXT NOT NULL,
+    "mode" "GameMode" NOT NULL DEFAULT 'NORMAL',
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "ready" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GameHistory" (
+    "id" UUID NOT NULL,
+    "gameId" UUID NOT NULL,
+    "player1Id" UUID NOT NULL,
+    "player2Id" UUID NOT NULL,
+    "mode" "GameMode" NOT NULL DEFAULT 'NORMAL',
+    "player1Score" INTEGER NOT NULL DEFAULT 0,
+    "player2Score" INTEGER NOT NULL DEFAULT 0,
+    "result" "GameResult" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "GameHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Channel" (
+    "id" UUID NOT NULL,
+    "title" TEXT NOT NULL,
+    "visibility" "ChannelVisibility" NOT NULL DEFAULT 'PUBLIC',
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Channel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Participant" (
+    "id" UUID NOT NULL,
+    "channelId" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "socketId" TEXT NOT NULL,
+    "role" "ParticipantRole" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ban" (
+    "id" UUID NOT NULL,
+    "channelId" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Ban_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Mute" (
+    "id" UUID NOT NULL,
+    "channelId" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Mute_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChatRoom" (
+    "id" UUID NOT NULL,
+    "srcId" UUID NOT NULL,
+    "destId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ChatRoom_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" UUID NOT NULL,
+    "chatRoomId" UUID NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_intraId_key" ON "User"("intraId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email2fa_key" ON "User"("email2fa");
+
+-- CreateIndex
+CREATE INDEX "intraId" ON "User"("intraId");
+
+-- CreateIndex
+CREATE INDEX "friendUserId" ON "Friend"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Friend_userId_friendId_key" ON "Friend"("userId", "friendId");
+
+-- CreateIndex
+CREATE INDEX "blockUserId" ON "Block"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Block_userId_blockedId_key" ON "Block"("userId", "blockedId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Game_userId_key" ON "Game"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Game_socketId_key" ON "Game"("socketId");
+
+-- CreateIndex
+CREATE INDEX "player1Id" ON "GameHistory"("player1Id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Participant_userId_key" ON "Participant"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Participant_socketId_key" ON "Participant"("socketId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Ban_channelId_userId_key" ON "Ban"("channelId", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Mute_channelId_key" ON "Mute"("channelId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Mute_userId_key" ON "Mute"("userId");
+
+-- CreateIndex
+CREATE INDEX "srcId" ON "ChatRoom"("srcId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ChatRoom_srcId_destId_key" ON "ChatRoom"("srcId", "destId");
+
+-- CreateIndex
+CREATE INDEX "chatRoomId" ON "Chat"("chatRoomId");
+
+-- AddForeignKey
+ALTER TABLE "Friend" ADD CONSTRAINT "Friend_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friend" ADD CONSTRAINT "Friend_friendId_fkey" FOREIGN KEY ("friendId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Block" ADD CONSTRAINT "Block_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Block" ADD CONSTRAINT "Block_blockedId_fkey" FOREIGN KEY ("blockedId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GameHistory" ADD CONSTRAINT "GameHistory_player1Id_fkey" FOREIGN KEY ("player1Id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GameHistory" ADD CONSTRAINT "GameHistory_player2Id_fkey" FOREIGN KEY ("player2Id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ban" ADD CONSTRAINT "Ban_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ban" ADD CONSTRAINT "Ban_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Mute" ADD CONSTRAINT "Mute_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Mute" ADD CONSTRAINT "Mute_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatRoom" ADD CONSTRAINT "ChatRoom_srcId_fkey" FOREIGN KEY ("srcId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatRoom" ADD CONSTRAINT "ChatRoom_destId_fkey" FOREIGN KEY ("destId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_chatRoomId_fkey" FOREIGN KEY ("chatRoomId") REFERENCES "ChatRoom"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
