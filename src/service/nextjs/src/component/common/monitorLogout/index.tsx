048451b13a3bb5f1bc16746c9403183dc8c2f7ed
Younganswer <dudtj1999@gmail.com>
2024-01-31 16:14:44 +0900

    [Fix] onbeforeunload

--- src/service/nextjs/src/component/common/monitorLogout/index.tsx
+++ src/service/nextjs/src/component/common/monitorLogout/index.tsx
@@ -11,19 +11,17 @@ const LogoutOnUnload = () => {
 	const { sockets } = useContext(SocketContext);
 
 	useEffect(() => {
-		if (sockets.gameSocket) {
-			window.onbeforeunload = () => {
-				if (pathname.indexOf('/game') === -1) {
-					sockets.gameSocket?.emit('leave', { pathname });
-				}
-				postFetcher('/user/offline').catch(async (error: any) => {});
-			};
-			router.events.on('routeChangeStart', () => {
-				if (pathname.indexOf('/game') === -1) {
-					sockets.gameSocket?.emit('leave', { pathname });
-				}
-			});
-		}
+		window.onbeforeunload = () => {
+			if (sockets.gameSocket && pathname.indexOf('/game') === -1) {
+				sockets.gameSocket?.emit('leave', { pathname });
+			}
+			postFetcher('/user/offline').catch(async (error: any) => {});
+		};
+		router.events.on('routeChangeStart', () => {
+			if (sockets.gameSocket && pathname.indexOf('/game') === -1) {
+				sockets.gameSocket?.emit('leave', { pathname });
+			}
+		});
 		return () => {
 			window.onbeforeunload = () => {};
 			router.events.off('routeChangeStart', () => {});
