c0d325fc0f363fd7612b50327102ced48b3e38fc
Younganswer <dudtj1999@gmail.com>
2024-01-31 14:55:42 +0900

    [Fix] Error message

--- src/service/nestjs/src/api/user/user.controller.ts
+++ src/service/nestjs/src/api/user/user.controller.ts
@@ -69,7 +69,7 @@ class UserController {
 			if (user.email2fa !== updateData.email2fa) {
 				user = await this.userService.findByEmail(updateData.email2fa);
 				if (user) {
-					throw new BadRequestException('Email is already taken');
+					throw new BadRequestException('Email used in 2fa is already taken');
 				}
 			}
 
